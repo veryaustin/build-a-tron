1fd24b6585b3ad2b459b893774553cb5d2e4a205
Austin Lauritsen <austin@veryaustin.com>
2015-12-17 10:26:48 -0600

    Cleanup comments in installer.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ curl --remote-name https://github.com/veryaustin/build-a-tron/.laptop.local
 
 # Clone Thoughtbot Laptop Script
 curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
+
+# Run thoughtbot laptop installer script
 less mac
 sh mac 2>&1 | tee ~/laptop.log
