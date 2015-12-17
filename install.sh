068316c633d7ea67a136f565072f417de9d71b1b
Austin Lauritsen <austin@veryaustin.com>
2015-12-17 14:10:54 -0600

    Remove thoughtbot mac file and display completion message.

--- install.sh
+++ install.sh
@@ -4,8 +4,16 @@
 cd $HOME/
 curl --remote-name https://raw.githubusercontent.com/veryaustin/build-a-tron/master/.laptop.local
 
-# Clone Thoughtbot Laptop Script
+# Clone thoughtbot Laptop Script
 curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
 
 # Run thoughtbot laptop installer script
 sh mac 2>&1 | tee ~/laptop.log
+
+#Cleanup/Verify
+brew doctor
+
+# Remove thoughtbot mac file
+rm $HOME/mac
+
+echo "Installation & Configuration Complete. Please see ~/laptop.log for errors."
